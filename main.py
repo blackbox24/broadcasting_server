8f7d72b5195d82ce774428a29234f255565f3fec
blackbox24 <nicardid@gmail.com>
2026-01-18 20:03:03 +0000

    changed project to cli

---
+++ main.py
@@ -0,0 +1,6 @@
+# TODO
+# bind ip add to server
+# ip address stored in file (clients.json)
+# connect to server
+# collect message
+# receive messages
---
+++ server.py
@@ -0,0 +1,2 @@
+# TODO
+# create a simple server that accepts connects
